c09aec43717c2d6292aecbbaf982fe60d2ce8c6f
GuiGui2401 <jguiyo@gmail.com>
2025-04-08 07:39:33 +0100

    change upload

--- src/components/upload/index.js
+++ src/components/upload/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Tabs, Modal, Image } from 'antd';
 import UploadMedia from './upload-media';
 import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
@@ -15,19 +15,29 @@ const MediaUpload = ({
   disabled = false,
 }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
+  const [activeTab, setActiveTab] = useState('item-1');
+
+  const showModal = useCallback(() => {
     setIsModalOpen(true);
-  };
-  const handleCancel = () => {
+  }, []);
+  
+  const handleCancel = useCallback(() => {
     setIsModalOpen(false);
-  };
-  const removeImg = (path) => {
+  }, []);
+
+  const removeImg = useCallback((path) => {
+    if (disabled) return;
     const nextArray = imageList.filter((item) => item !== path);
     form.setFieldsValue({
       images: nextArray,
     });
     setImageList(nextArray);
-  };
+  }, [disabled, imageList, form, setImageList]);
+
+  const handleTabChange = useCallback((key) => {
+    setActiveTab(key);
+  }, []);
+
   return (
     <>
       <div className='media-upload-wrapper'>
@@ -35,24 +45,29 @@ const MediaUpload = ({
           <div
             key={item}
             className='image-wrapper'
-            onClick={() => {
-              if (!disabled) {
-                removeImg(item);
-              }
-            }}
+            onClick={() => removeImg(item)}
+            role="button"
+            tabIndex={0}
+            aria-label="Remove image"
           >
             <Image
               preview={false}
               src={getImage(item?.name)}
               className='images'
-              alt={'images'}
+              alt={'Uploaded image'}
             />
-            {!disabled && <DeleteOutlined />}
+            {!disabled && <DeleteOutlined aria-hidden="true" />}
           </div>
         ))}
         {(multiple || !imageList.length) && !disabled && (
-          <div className='media-upload' onClick={showModal}>
-            <PlusOutlined /> <span>Upload</span>
+          <div 
+            className='media-upload' 
+            onClick={showModal}
+            role="button"
+            tabIndex={0}
+            aria-label="Upload media"
+          >
+            <PlusOutlined aria-hidden="true" /> <span>Upload</span>
           </div>
         )}
       </div>
@@ -60,11 +75,27 @@ const MediaUpload = ({
         onCancel={handleCancel}
         maskClosable={true}
         visible={isModalOpen}
-        footer={false}
+        footer={null}
         width={'80%'}
+        destroyOnClose
+        // Ajout d'attributs d'accessibilité
+        aria-labelledby="media-upload-modal-title"
       >
-        <Tabs>
-          <Tabs.TabPane tab='Media files' key='item-1'>
+        <h2 id="media-upload-modal-title" className="sr-only">Media Upload</h2>
+        <Tabs 
+          activeKey={activeTab}
+          onChange={handleTabChange}
+          // Options d'accessibilité
+          tabBarGutter={10}
+          accessibility={{
+            hideOnBlur: false
+          }}
+        >
+          <Tabs.TabPane 
+            tab='Media files' 
+            key='item-1'
+            forceRender
+          >
             <UploadMedia
               form={form}
               setImageList={setImageList}
@@ -77,6 +108,7 @@ const MediaUpload = ({
             tab='Upload media'
             key='item-2'
             className='upload-media'
+            forceRender
           >
             <ImageGallery
               type={type}
@@ -92,4 +124,5 @@ const MediaUpload = ({
     </>
   );
 };
-export default MediaUpload;
+
+export default React.memo(MediaUpload);
